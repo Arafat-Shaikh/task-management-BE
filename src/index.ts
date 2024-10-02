12f5fe3363a9eceaaa4761898596cad6a164446e
Arafat <arafatshaikh823@gmail.com>
2024-10-02 17:31:24 +0530

    pre deploy 6

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ declare global {
   }
 }
 const port = process.env.PORT || 3000;
+const url = process.env.ORIGIN_URL || "http://localhost:3000";
 const app = express();
 
 app.use(cookieParser());
@@ -24,7 +25,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: url,
   })
 );
 
